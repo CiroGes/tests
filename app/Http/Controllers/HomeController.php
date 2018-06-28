60a967fa75710779c945b8afdf01a7cdc74ee5a0
German <german.sp91@gmail.com>
2018-06-28 15:52:06 -0300

    TG-8 #in-progress este commit soluciona bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller {
 
   public function index() {
-      if (true) {
-          return view('index');
-      }
+      return view('index');
   }
 
   public function form() {
